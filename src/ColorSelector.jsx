5c653e144ec65ea7d587b8a6e752e898a9310cbe
Snusii <Snusii1995@gmail.com>
2025-05-05 14:27:40 +0200

    Added Anonymous functions on the buttons in ColorSelector.jsx (fixed problem from yesterday)

--- src/ColorSelector.jsx
+++ src/ColorSelector.jsx
@@ -4,10 +4,10 @@ export default function ColorSelector ({ setColor }) {
     
     return(
         <div id="ColorSelector">
-            <button onClick={setColor('Blue') }>Blue</button>
-            <button onClick={setColor('Pink') }>Pink</button>
-            <button onClick={setColor('Purple') }>Purple</button>
-            <button onClick={setColor('Black') }>Black</button>
+            <button onClick={() => setColor('Blue') }>Blue</button>
+            <button onClick={() => setColor('Pink') }>Pink</button>
+            <button onClick={() => setColor('Purple') }>Purple</button>
+            <button onClick={() => setColor('Black') }>Black</button>
         </div>
     )
 }
